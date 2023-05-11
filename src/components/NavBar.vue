5c5d40d22f02628c27663636b5724f6a518edb97
ZheNing Hu <adlternative@gmail.com>
2023-05-11 17:38:32 +0800

    feat(home): create home page and nav bar
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

---
+++ src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav>
+    <ul class="navbar-list">
+      <li>
+        <router-link to="/">
+          <img src="../assets/logo.png" class="navbar-logo">
+        </router-link>
+      </li>
+      <li>
+        <div class="navbar-right">
+          <router-link class="navbar-item" to="/login">Login</router-link>
+          <router-link class="navbar-item" to="/register">Register</router-link>
+        </div>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+nav {
+  background-color: #333;
+  color: #fff;
+  height: 80px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+}
+
+.navbar-item {
+  color: #fff;
+  font-size: 24px;
+  padding: 0 20px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  font-size: 28px; /* 增加字体大小 */
+}
+
+.navbar-list {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  height: 100%;
+}
+
+.navbar-right {
+  display: flex;
+  align-items: center;
+}
+
+.router-link-active {
+  font-weight: bold;
+}
+
+.navbar-logo {
+  max-height: 5%;
+  max-width: 5%;
+  height: auto;
+  width: auto;
+  border-radius: 50%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="../assets/logo.png" class="logo">
+    <div class="tiny-github">
+      <h1>TinyGithub</h1>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.logo {
+  width: 250px;
+  height: 250px;
+  margin-bottom: 300px;
+}
+
+.tiny-github {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 5rem;
+  font-weight: bold;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin-top: 100px;
+}
+</style>
