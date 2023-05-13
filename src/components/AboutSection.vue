13a8af47f6f3f9f4c8ae001b1e371c8f865585f2
ZheNing Hu <adlternative@gmail.com>
2023-05-13 21:23:56 +0800

    feat(about): create about section
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about-section">
+    <div class="title">
+      <h2>About</h2>
+    </div>
+    <div>{{ description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.about-section {
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+
+.title {
+  display: flex;
+  align-items: flex-start;
+}
+</style>
--- src/views/RepoView.vue
+++ src/views/RepoView.vue
@@ -1,22 +1,73 @@
 <template>
   <div class="repo-view-container">
-    <div class="repo-name-line">
-      <repo-name-line :username="username" :reponame="reponame" />
-    </div>
-    <div class="git-blam-tree">
-      <git-blame-tree :username="username" :reponame="reponame" :treepath="'/'" />
+    <div v-if="!isLoaded">Loading...</div>
+    <div v-else>
+      <div class="repo-name-line">
+        <repo-name-line :username="username" :reponame="reponame" />
+      </div>
+      <div class="about-section">
+        <about-section :description="repository.Desc" />
+      </div>
+      <div class="git-blame-tree">
+        <git-blame-tree :username="username" :reponame="reponame" :treepath="'/'" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import router from '@/router';
 import GitBlameTree from '@/components/GitBlameTree.vue';
 import RepoNameLine from '@/components/RepoNameLine.vue';
+import AboutSection from '@/components/AboutSection.vue';
 export default {
   name: 'RepoView',
   components: {
     RepoNameLine,
     GitBlameTree,
+    AboutSection,
+  },
+  data() {
+    return {
+      isLoaded: false,
+    }
+  },
+  created() {
+    this.$watch(
+      () => this.$route.params,
+      () => {
+        this.fetchData()
+      },
+      // 组件创建完后获取数据，
+      // 此时 data 已经被 observed 了
+      { immediate: true }
+    )
+  },
+  methods: {
+    fetchData() {
+      this.isLoaded = false;
+
+      axios.get(`/api/v2/${this.$route.params.username}/${this.$route.params.reponame}`)
+        .then(response => {
+          // 从响应中获取邮箱数据
+          this.isLoaded = true;
+          this.repository = response.data.repo
+        })
+        .catch(error => {
+          if (error.response) {
+            if (error.response.status === 404) {
+              // 路由跳转到 NotFound 页面
+              router.push('/404');
+            } else {
+              this.$emit('show-error', error.response.data.error)
+            }
+          } else {
+            this.$emit('show-error', error.message)
+          }
+          this.isLoaded = true;
+        });
+    }
   },
   computed: {
     username() {
@@ -47,5 +98,14 @@ export default {
   flex-grow: 1;
   left: 50px;
   margin-left: 50px;
+}
+
+.about-section {
+  position: fixed;
+  top: 15%;
+  right: 20px;
+  width: 35%;
+  max-width: 50%;
+  bottom: 30px;
 }
 </style>
